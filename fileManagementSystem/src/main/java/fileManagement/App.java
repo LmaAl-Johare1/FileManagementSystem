f00078d599fc3519f5e583f73b09e3ef7601e60a
LmaAl-Johare1 <lmaref.j@gmail.com>
2022-12-20 20:02:08 +0200

    no message

--- fileManagementSystem/src/main/java/fileManagement/App.java
+++ fileManagementSystem/src/main/java/fileManagement/App.java
@@ -1,16 +1,17 @@
 package fileManagement;
-
 import fileManagement.service.DeleteFile;
 import fileManagement.service.ImportFile;
 
 import java.io.IOException;
 
+import static fileManagement.service.DeleteFile.deleteFile;
+//import static fileManagement.service.VersionControl.versionControl;
+
 public class App {
     public static void main(String[] args) throws IOException {
-
-
-       ImportFile.ImportFile();
-        DeleteFile.deleteFile();
-
+      ImportFile.ImportFile();
+       DeleteFile.deleteFile();
+        //versionControl();
+        deleteFile();
     }
 }
