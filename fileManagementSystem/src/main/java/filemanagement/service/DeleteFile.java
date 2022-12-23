d10637cd63fdcb817f708d6f93a02ede13590384
noorkobari <noor.kobari9@gmail.com>
2022-12-23 06:09:53 +0200

    add exception

--- fileManagementSystem/src/main/java/filemanagement/service/DeleteFile.java
+++ fileManagementSystem/src/main/java/filemanagement/service/DeleteFile.java
@@ -8,39 +8,45 @@ import org.json.JSONObject;
 public class DeleteFile extends GetFile {
     static boolean removed;
     static JSONArray innerArray=null;
+    static int index;
+    static String filename;
 
     public static void deleteFile() throws JsonReadingException, NoFileException {
+        readJsonFile();
+        JSONArray filesArray = jsonObject.getJSONArray("files");
+
         Scanner scanner = new Scanner(System.in);
         System.out.print("\n Enter file name you want to delete (ex:file.txt): ");
         String nameWithType = scanner.nextLine();
-        String filename = nameWithType.substring(0, nameWithType.lastIndexOf('.'));
-        // Read the JSON file and parse it into a Java object
-        readJsonFile();
-        JSONArray filesArray = jsonObject.getJSONArray("files");
-        // Find the index of the file with the specified name
-        if (filesArray.length()==0) {
+        try {
+            String filename = nameWithType.substring(0, nameWithType.lastIndexOf('.'));
+        } catch (Exception e) {
             throw new NoFileException();
         }
-        for (int i = 0; i < filesArray.length(); i++) {
-             innerArray = filesArray.getJSONArray(i);
-            JSONObject objFile = innerArray.getJSONObject(0);
-            String fileNameDb = objFile.getString("fileName");
-            String fileTypeDb = objFile.getString("fileType");
-                if ((filename.equals(fileNameDb))&& (getExtension(nameWithType).equals(fileTypeDb)) ) {
-                    removed=!isRemoved();
+
+        // Find the index of the file with the specified name
+        if (filesArray.length() == 0) {
+            throw new NoFileException();
+        }
+        else {
+            for (int i = 0; i < filesArray.length(); i++) {
+                innerArray = filesArray.getJSONArray(i);
+                JSONObject objFile = innerArray.getJSONObject(0);
+                String fileNameDb = objFile.getString("fileName");
+                String fileTypeDb = objFile.getString("fileType");
+                if ((filename.equals(fileNameDb)) && (getExtension(nameWithType).equals(fileTypeDb))) {
+                    removed = false;
+                    index = i;
                     break;
                 }
             }
-        if(removed){
-            innerArray.remove(0);
-            Logger.logInfo("File deleted successfully \n");
-           updateJsonData(jsonObject);
+            if (removed) {
+                filesArray.remove(index);
+                Logger.logInfo("File deleted successfully \n");
+                updateJsonData(jsonObject);
+            } else {
+                throw new NoFileException();
+            }
         }
-        else
-            Logger.logError("File doesn't exist \n");
-
     }
-        private static boolean isRemoved() {
-            return false;
-        }
 }
