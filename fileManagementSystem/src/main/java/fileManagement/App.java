f97c86205e3babddfc93cec426b0f9ba9a5d3ada
LmaAl-Johare1 <lmaref.j@gmail.com>
2022-12-26 01:13:41 +0200

    no message

--- fileManagementSystem/src/main/java/fileManagement/App.java
+++ fileManagementSystem/src/main/java/fileManagement/App.java
@@ -3,6 +3,7 @@ package filemanagement;
 import filemanagement.exception.NoDataInFileJsonException;
 import filemanagement.exception.UserNotFoundException;
 import filemanagement.exception.NameNotFoundException;
+import filemanagement.permission.service.classification.ClassifyByType;
 import filemanagement.userauthenticate.ReadUserInfoFromJson;
 import filemanagement.userauthenticate.UserLogin;
 
@@ -10,13 +11,13 @@ import java.io.IOException;
 
 
 public class App {
-    public static void main(String[] args) throws IOException, UserNotFoundException, NameNotFoundException, NoDataInFileJsonException {
+    public static void main(String[] args) throws IOException, NameNotFoundException, NoDataInFileJsonException, UserNotFoundException {
 
         ReadUserInfoFromJson readInfo = ReadUserInfoFromJson.getInstance();
         System.out.println(readInfo.getUserInfoFromJson());
         UserLogin userLogin = UserLogin.getInstance();
         userLogin.logIn(readInfo);
-    
 
-}
+
+    }
 }
